39d73362cdc0a54ddb8c94ab17e055656b1b4641
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-09 20:43:40 +0200

    Add public folder that will contain css file, also tell express where to find it using the express.static middleware.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const usersRouter = require("./routes/usersRouter");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));

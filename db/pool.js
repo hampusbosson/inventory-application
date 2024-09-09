4adea15d2f90e0d3e8c2fccedd0da268d376c135
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-09 19:26:24 +0200

    Add function to execute queries.

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,14 @@
 const { Pool } = require("pg");
 require("dotenv").config(); // Load environment variables
 
-module.exports = new Pool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+const query = (text, params) => pool.query(text, params);
+
+module.exports = { query };

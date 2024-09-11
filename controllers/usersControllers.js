99e0e3e7fa8a716d0f65a11f3eea712b990431b3
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-11 23:34:30 +0200

    Create addNewMovie function that adds a movie to the db from the form

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,58 +1,103 @@
 const db = require("../db/queries");
-const {body, validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
-const pageTitle = "MovieHub"
+const pageTitle = "MovieHub";
+const movieLengthErr = "must be between 1 and 30 charachters";
+const desctiptionLengthErr = "must be between 1 and 120 charachters";
+
+const validateMovie = [
+  body("movie-name")
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .withMessage(`Movie name ${movieLengthErr}`),
+  body("description")
+    .trim()
+    .isLength({ min: 1, max: 120 })
+    .withMessage(`Desctiption ${desctiptionLengthErr}`),
+];
 
 const getHomePage = (req, res) => {
-    try {
-        res.render("index", {
-            title: pageTitle,
-        });
-    } catch(err) {
-        console.error("Failed to load page: ", {err});
-        res.status(500).send("an error occured while loading the page.")
-    }
-}
+  try {
+    res.render("index", {
+      title: pageTitle,
+    });
+  } catch (err) {
+    console.error("Failed to load page: ", { err });
+    res.status(500).send("an error occured while loading the page.");
+  }
+};
 
 const getMoviePage = (req, res) => {
-    try {
-        res.render("movies", {
-            title: pageTitle,
-        })
-    } catch(err) {
-        console.error("Failder to load page: ", err); 
-        res.status(500).send("an error occured while loading the page.")
-    }
-}
+  try {
+    res.render("movies", {
+      title: pageTitle,
+    });
+  } catch (err) {
+    console.error("Failder to load page: ", err);
+    res.status(500).send("an error occured while loading the page.");
+  }
+};
 
 const getGenresPage = (req, res) => {
-    try {
-        res.render("genres", {
-            title: pageTitle,
-        })
-    } catch(err) {
-        console.error("Failder to load page: ", err); 
-        res.status(500).send("an error occured while loading the page.")
-    }
-}
+  try {
+    res.render("genres", {
+      title: pageTitle,
+    });
+  } catch (err) {
+    console.error("Failder to load page: ", err);
+    res.status(500).send("an error occured while loading the page.");
+  }
+};
 
 const getNewMoviePage = (req, res) => {
+  try {
+    res.render("newMovie", {
+      title: pageTitle,
+    });
+  } catch (err) {
+    console.error("Failder to load page: ", err);
+    res.status(500).send("an error occured while loading the page.");
+  }
+};
+
+const addNewMovie = [
+  validateMovie,
+  async (req, res) => {
     try {
-        res.render("newMovie", {
-            title: pageTitle,
-        })
-    } catch(err) {
-        console.error("Failder to load page: ", err); 
-        res.status(500).send("an error occured while loading the page.")
+      const {
+        name,
+        genres,
+        description,
+        posterURL,
+        trailerURL,
+        rating, 
+        duration, 
+        year 
+      } = req.body;
+
+      await db.insertMovie(
+        name,
+        year,
+        genres,
+        rating,   
+        duration, 
+        description,
+        posterURL,
+        trailerURL,
+      );
+
+      res.redirect("/movies");
+    } catch (err) {
+      console.error("Error creating movie: ", err);
+      res.status(500).send("Error creating movie");
     }
-}
+  },
+];
 
 module.exports = {
-    getHomePage,
-    getMoviePage,
-    getGenresPage,
-    getNewMoviePage
-}
-
-
-
+  getHomePage,
+  getMoviePage,
+  getGenresPage,
+  getNewMoviePage,
+  addNewMovie,
+};

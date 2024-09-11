fe24b6d6aa3696e8f15b4ad285660ab447b03076
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-11 23:35:37 +0200

    Make post path from form absolute instead of relative.

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,6 +6,7 @@ userRouter.get("/", usersController.getHomePage);
 userRouter.get("/movies", usersController.getMoviePage);
 userRouter.get("/genres", usersController.getGenresPage);
 userRouter.get("/movies/new", usersController.getNewMoviePage);
+userRouter.post("/movies/new", usersController.addNewMovie);
 
 module.exports = userRouter; 
 
